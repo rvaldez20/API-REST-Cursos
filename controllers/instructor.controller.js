ace48552c7bb45c73e0eaf17600d05ec978f7785
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-01-14 18:51:21 -0600

    Se crea el end-point para obtener un instructor por ID

--- controllers/instructor.controller.js
+++ controllers/instructor.controller.js
@@ -1,6 +1,7 @@
 // importamos el modelo instructor
 const Instructor = require('../models/Instructor');
 
+// Para obtener todos los instructors
 exports.getInstructors = async (req, res, next) => {
    try {
       const instructors = await Instructor.findAll({});
@@ -11,6 +12,28 @@ exports.getInstructors = async (req, res, next) => {
    }
 }
 
+// Para obtener un instructor por ID
+exports.getInstructor = async (req, res, next) => {
+   const { idInstructor } = req.params;
+   try {
+      const instructor = await Instructor.findOne({
+         where: {
+            id: idInstructor
+         }
+      })
+      if(!instructor) {
+         res.json({ message: 'El instructor no esta registrado'});
+         return next();
+      }
+
+      res.json(instructor);
+   } catch (error) {
+      console.log(error);
+      next();
+   }
+}
+
+// Para agregar un nuevo instructor
 exports.addInstructor = async (req, res, next) => {
    const { name, url } = req.body;  
    try {
--- routes/instructor.routes.js
+++ routes/instructor.routes.js
@@ -3,13 +3,18 @@ const router = express.Router();
 
 const instructorController = require('../controllers/instructor.controller');
 
-module.exports = function () {
+module.exports = function () {   
+
+   // Ruta para obtener todos los instructores
+   router.get('/instructor', instructorController.getInstructors);
+
+   // Ruta para obtener un instructor por ID
+   router.get('/instructor/:idInstructor', instructorController.getInstructor);
 
    // Ruta para agregar un instructor
    router.post('/instructor', instructorController.addInstructor);
 
-   // Ruta para obtener todos los instructores
-   router.get('/instructor', instructorController.getInstructors);
+   
 
 
    return router;
